2481f7399926d6bcd89616e03dbd2ec8f81b5ca5
Lukisjama <lukisjama@gmail.com>
2020-12-06 09:56:52 +0100

    05 day

---
+++ 05/05_part2.py
@@ -0,0 +1,45 @@
+
+puzzle = []
+unique = {""}
+count = 0
+suma = 0
+people = 0
+
+with open('05.txt', 'r') as file:
+    rawPuzzle = file.read().replace('\n', ' ')
+
+puzzle = rawPuzzle.split("  ")
+print (puzzle)
+
+for group in puzzle:
+    print (group)
+    people = len(group.split(" "))
+    print ("Počet lidí: " + str(people))
+    for char in group:
+        if (char != ' '):
+            count = group.count(char);
+            print ("Počet písmen: '" + char + "' je " + str(count))
+            if count == people:
+                print ("-Všichni odpověděli")
+                if char not in unique:
+                    print("--Ještě jsem nezapočítal")
+                    unique.add(char)
+                    suma += 1
+                    print ("Suma je " + str(suma))
+                else:
+                    print ("--Už znám")
+    unique.clear()
+    print ("_______")
+
+
+
+print(suma)
+
+
+#for line in open('05.txt', 'r').readlines():
+    #print(line)
+    #if "//n//n" not in line:
+        #print("______")
+
+
+#print(len(set("aaaa")))
