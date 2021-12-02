8d7060c0fc872b7d0c85eff6c1a8071ce9b2ffb7
marek <lukisjama@gmail.com>
2021-12-02 17:30:21 +0100

    reorder files

--- 2021/01/main.py
+++ 2021/01/main.py
@@ -10,7 +10,13 @@ for line in open('01.txt', 'r').readlines():
 for i in range(len(num)-1):
     if num[i+1]>num[i]:
         inc+=1
-        print("Increment: " + str(inc))
+        #print("Increment: " + str(inc))
 
+print ("First part: " + str(inc))
+inc = 0
 
-
+for i in range(windows_size + 1, len(num) + 1):
+    if sum(num[i-windows_size:i]) > sum(num[i - windows_size-1:i-1]):
+        #print(str(num[i - windows_size:i]) + " > " + str(num[i - windows_size - 1:i - 1]))
+        inc += 1
+print ("Second part: " + str(inc))
