bb2163c73b8179ae215def17ee598efae1d6b668
marek <lukisjama@gmail.com>
2021-12-02 18:13:24 +0100

    reorder files

--- 2021/02/main.py
+++ 2021/02/main.py
@@ -1,6 +1,21 @@
-depth = 0
-aim = 0
-horizontal = 0
+with open('02.txt','r') as f:
+    lines = f.readlines()
 
-for line in open('02.txt', 'r').readlines():
-    print("Hello World")
+    v2 = 0
+    v1 = 0
+    horizontal = 0
+    aim = 0
+    for line in lines:
+        num = int(line.split(' ')[1])
+        if line.startswith('up'):
+            aim -= num
+            v1 -= num
+        if line.startswith('down'):
+            aim += num
+            v1 += num
+        if line.startswith('forward'):
+            horizontal += num
+            v2 += aim*num
+
+print(horizontal * v1)
+print(horizontal * v2)
